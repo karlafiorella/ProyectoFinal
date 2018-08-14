9a70f835a6838e9efa35f763eb7392bb6fc426ab
karlafiorella <karla.espinoza@gmail.com>
2018-08-14 18:54:20 -0400

    mitercercommit

---
+++ database/migrations/2018_08_14_220957_create_carrusels_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarruselsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carrusels', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('title');
+            $table->mediumText('description');
+            $table ->string('file')->nullable()->default('sin-imagen.jpg');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carrusels');
+    }
+}
