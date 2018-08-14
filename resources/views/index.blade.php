05d39d2e41402cc82c8dd8beed70b4a802043860
karlafiorella <karla.espinoza@gmail.com>
2018-08-14 17:43:54 -0400

    misegundocommit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.principal')
+
+@section('content') 
+
+	@include(layouts.intro);
+	@include(layouts.featured);
+	@include(layouts.about);
+	@include(layouts.services);
+	@include(layouts.call);
+	@include(layouts.skills);
+	@include(layouts.facts);
+	@include(layouts.portfolio);
+	@include(layouts.clients);
+	@include(layouts.contact);
+
+@endsection
